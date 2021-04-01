674d54ac1e03616b72d070a142eaa57e660a62fa
Ades-Angel <64943306+Ades-Angel@users.noreply.github.com>
2021-04-01 11:48:13 +0700

    Update Pre-Release-Task1.py

--- Pre-Release-Task1.py
+++ Pre-Release-Task1.py
@@ -1,20 +1,35 @@
+books = []
+
 def get_input():
-    book_list = []
+    record = []
     print('This a book collection database. Please enter 10 books.')
-    for x in range(0,2):
-        code = input('Please enter the Code: ')
+    for count in range(2):
+        code = int(input('Please enter the Code between 100 and 999: '))
+        while not (code >= 100 and code<= 999 and is_unique(count, code)):
+            code = int(input('Please enter the code proprely: '))
         title = input('Please enter the Title: ')
         author = input('Please enter the Author: ')
         year = input('Please enter the Year: ')
         print('\n')
-        book_record = code + "_" + title + "_" + author + "_" + year
-        book_list.append(book_record)
+        book_record = str(code) + "_" + title + "_" + author + "_" + year
+        record.append(book_record)
+        books.append(record)
     with open('Book_Collection.txt','a') as file:
         file.writelines("\n")
-        file.writelines(book_list)
-    for i in book_list:
-        print('\n')
-        print(book_list[i])
+        file.writelines(book_record)
 
 
+def is_unique(count, code):
+    if count == 0:
+        return True
+    else:
+        return not (code in books[0])
+
+
+def display_data():
+    print(" Book ID | Book Title \t\t | Author \t\t| Year of Publication |")
+    for row in range(len(books)):
+        print("\t", books[row][0], " |\t\t", books[row][1], "\t\t|", books[row][2], "\t\t |\t\t", books[row][3])
+    
 get_input()
+display_data()
