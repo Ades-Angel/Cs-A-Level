be5723806c4c4ba5ecacd8742d3de902729e2f48
Ades-Angel <64943306+Ades-Angel@users.noreply.github.com>
2021-04-01 12:37:07 +0700

    Update Pre-Release-Task1.py

--- Pre-Release-Task1.py
+++ Pre-Release-Task1.py
@@ -1,9 +1,9 @@
 books = []
 
 def get_input():
-    record = []
     print('This a book collection database. Please enter 10 books.')
     for count in range(2):
+        record = []
         code = int(input('Please enter the Code between 100 and 999: '))
         while not (code >= 100 and code<= 999 and is_unique(count, code)):
             code = int(input('Please enter the code proprely: '))
@@ -12,11 +12,9 @@ def get_input():
         year = input('Please enter the Year: ')
         print('\n')
         book_record = str(code) + "_" + title + "_" + author + "_" + year
+        write_to_file(book_record)
         record.append(book_record)
         books.append(record)
-    with open('Book_Collection.txt','a') as file:
-        file.writelines("\n")
-        file.writelines(book_record)
 
 
 def is_unique(count, code):
@@ -30,6 +28,35 @@ def display_data():
     print(" Book ID | Book Title \t\t | Author \t\t| Year of Publication |")
     for row in range(len(books)):
         print("\t", books[row][0], " |\t\t", books[row][1], "\t\t|", books[row][2], "\t\t |\t\t", books[row][3])
-    
+
+
+def write_to_file(book_record):
+    file=open("Library.txt", "+a")
+    file.writelines(book_record)
+    file.writelines("\n")
+    file.close()
+
+def read_file():
+    file=open("Library.txt", "r")
+    print("Book ID | Book Title \t\t | Author \t\t| Year of Publication")
+    for read_lines in file:
+        book_records = read_lines.split("_")
+        print(book_records[0],"  ","| ", book_records[1], "\t\t |", book_records[2], "      |", book_records[3])
+
+hash_table = [[] for x in range(10)]
+
+def insert(hash_table, code, title, author, year):
+    hash_key = hash(code) % len(hash_table)
+
+
+def search():
+    hash_key = hash(key) % len(hash_table)
+    slots = hash_table[hash_key]
+    for i. abcd in enumerate(slots):
+        a, b, c, d= abcd
+        if key == a:
+            return b
+
+
 get_input()
-display_data()
+#read_file()
